bfcb4aca039cf6c4005c2f8e37650b8d12142797
vagrant <vagrant@localhost.localdomain>
2020-07-13 09:53:12 +0000

    提出課題

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,57 @@
+class BooksController < ApplicationController
+    def top
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+    redirect_to book_path(@book)
+    flash[:notice] = "Book was successfully created."
+    else
+    @books = Book.all
+    render :index
+    end
+  end
+
+  def edit
+    @book = Book.find_by(id: params[:id])
+  end
+
+  def update
+    @book = Book.find_by(id: params[:id])
+    if @book.update(book_params)
+    redirect_to book_path(@book)
+    flash[:notice] = "Book was successfully updated."
+    else
+    render :edit
+    end
+  end
+
+   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+      if book.destroy
+        flash[:notice] = "Book was successfully destroyed."
+      end
+    redirect_to ('/books')
+   end
+
+  private
+
+    def book_params
+        params.require(:book).permit(:title, :body)
+    end
+end
